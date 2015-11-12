0ef111ce183ca200837c305c88640445d15d14dd
Jhonatan Laguna <Jhonatan Laguna>
2015-11-12 09:05:28 -0700

    Archivos Base

---
+++ Calaca.Nucleo/Entidades/Usuario.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Calaca.Nucleo.Persistencia;
+using NHibernate;
+
+
+namespace Calaca.Nucleo.Entidades
+{
+    public class Usuario : Persistent
+    {
+        public override int Id { get; set;}
+        public string Nombre { get; set; }
+        public string Correo { get; set; }
+
+        public static IList<Usuario> ObtenerTodos()
+        {
+            IList<Usuario> usuarios = new List<Usuario>();
+
+            try
+            {
+                using (ISession session = Persistent.SessionFactory.OpenSession())
+                {
+                    ICriteria crit = session.CreateCriteria(new Usuario().GetType());
+                    usuarios = crit.List<Usuario>();
+                }
+            }
+            catch(Exception e)
+            {
+                throw e;
+            }
+
+            return usuarios;
+        }
+
+    }
+}
---
+++ Calaca.Presentacion/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using Calaca.Nucleo.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Calaca.Presentacion.Controllers
+{
+    public class HomeController : Controller
+    {
+        
+            
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            IList<Usuario> users = Usuario.ObtenerTodos();
+            ViewBag.Usuarios = users;
+            return View();
+        }   
+
+
+        public ActionResult Registro()
+        {
+            return View();
+        }
+
+        public ActionResult Guardar(Usuario u)
+        {
+            u.Save();
+            IList<Usuario> users = Usuario.ObtenerTodos();
+            ViewBag.Usuarios = users;
+            return View("Index");
+        }
+
+    }
+}
